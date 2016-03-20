83b504aa97b1fa7e6b84a20c7c049fe2e46fa224
MaxLevs <maxlevs@mail.ru>
2016-03-20 17:25:28 +1000

    Начал писать основу. Проверка исполнения в OCOS

--- main_v1.lua
+++ main_v1.lua
@@ -1,2 +1,10 @@
+--Подключение библиотек
 local component = require("component")
 local computer = require("computer")
+local nav = component.navigation
+local fs = require("filesistem")
+
+--Основные переменные
+local x,y,z = nav.getPosition()
+
+print(x,y,z)
