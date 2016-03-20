ac01e5c3c20e9d6ac798b8bcc3840af61b79c2fb
MaxLevs <maxlevs@mail.ru>
2016-03-20 21:42:51 +1000

    Добавил несколько MUST-HAVE функций: *Чтение-запись в текстовую БД *Пресдтавление таблицы в текстовом формате (Под LUA) *Начало создания четкой структуры файла БД

--- main_v1.lua
+++ main_v1.lua
@@ -6,5 +6,81 @@ local fs = require("filesystem")
 
 --Основные переменные
 local x,y,z = nav.getPosition()
+local facing = nav.getFacing()
+local mapRange = nav.getRange()
 
-print(x,y,z)
+
+-----------------------------
+------MUST-HAVE функции------
+-----------------------------
+
+function table.val_to_str ( v )
+	if "string" == type( v ) then
+		v = string.gsub( v, "\n", "\\n" )
+		if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
+			return "'" .. v .. "'"
+		end
+		return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
+	end
+	return "table" == type( v ) and table.tostring( v ) or tostring( v )
+end
+
+function table.key_to_str ( k )
+	if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
+		return k
+	end
+	return "[" .. table.val_to_str( k ) .. "]"
+end
+
+-- Преобразование таблицы или массива в текстовое представление в соответствии с синтаксисом языка lua
+function table.tostring( tbl )
+	local result, done = {}, {}
+	for k, v in ipairs( tbl ) do
+		table.insert( result, table.val_to_str( v ) )
+		done[ k ] = true
+	end
+	for k, v in pairs( tbl ) do
+		if not done[ k ] then
+			table.insert( result, table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
+		end
+	end
+	return "{" .. table.concat( result, "," ) .. "}"
+end
+
+
+
+
+--Функции работы с текстовой БД
+local function dbGet()
+	local f, err = io.open("db.txt", "r")
+	if not f then return nil, err end
+	local tbl = assert(loadstring("return " .. f:read("*a")))
+	f:close()
+	return tbl()
+end
+
+local function dbSet(data)
+	local tbl = dbGet()
+	if tbl then
+		table.insert(tbl, data)
+	else tbl = data
+	end
+
+	local f, err = io.open("db.txt", "w")
+	if not f then return nil, err end
+	f:write(table.tostring(tbl))
+	f:close()
+	return true
+end
+
+local function dbSet(data)
+	local f, err = io.open("db.txt", "w")
+	if not f then return nil, err end
+	f:write(table.tostring(data))
+	f:close()
+	return true
+end
+
+local function logSet(x, y, z, r, ... )
+	
+end
